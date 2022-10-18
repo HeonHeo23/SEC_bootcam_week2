83593fd16a828b1109e76925abb1727aa324a6e5
HeonHeo23 <chrisheo23@gmail.com>
2022-10-19 03:55:47 +0900

    finished js24

---
+++ JS24/main.js
@@ -0,0 +1,12 @@
+// import playGuitar from "./guitar.js";
+// import { shredding as shred, plucking as fingerpicking } from "./guitar.js";
+import * as Guitars from "./guitar.js"
+import User from './user.js';
+
+const me = new User("email@email.com", "Dave");
+console.log(me);
+console.log(me.greeting());
+
+console.log(Guitars.playGuitar());
+console.log(Guitars.shredding());
+console.log(Guitars.plucking());
